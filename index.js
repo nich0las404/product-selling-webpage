3bc2b598727e352cd0d675c148f94359b45ef60b
nicho2007 <nicholashgan@gmail.com>
2024-09-16 20:57:16 +0700

    FEAT: Added animation using intersctionObserver() in all the scroller class and discount section

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ function changeBackgroundGif() {
 }
 backgroundGif.style.backgroundImage = gifUrls[currentIndex];
 setInterval(changeBackgroundGif, 5000);
+
+// ANIMATION WHEN USER SCROLLS DOWN ---- 14 September 2024
+document.addEventListener('DOMContentLoaded', () => {
+const categoryButtons = document.querySelectorAll('.category-btn');
+const discountContainer = document.querySelector('.discount-container');
+const scroller = document.querySelectorAll('.scroller')
+    // Create an IntersectionObserver
+const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target); // Stop observing once the element is in view
+            }
+        });
+    }, {
+        threshold: 0.2 // Trigger when 20% of the element is in view
+    });
+
+    // Observe each category button
+    categoryButtons.forEach(button => {
+        observer.observe(button);
+    });
+    observer.observe(discountContainer);
+    scroller.forEach(scrolls => 
+        observer.observe(scrolls)
+    )
+});
+
 // MAIN: TIMER FOR DISCOUNTS ---- 8 September 2024
 const timer = document.querySelector('.discount-timer')
 let countdown = 18 * 60 * 60
@@ -19,7 +47,7 @@ function discountTimer(){
     // 18:00:00
 
     const hour = Math.floor(countdown / 3600)
-    const minutes = Math.floor(Math.floor(countdown % 3600) / 60)
+    const minutes = Math.floor((countdown % 3600) / 60)
     const seconds = countdown % 60
 
     timer.textContent = `${hour < 10? '0': ''}${hour}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
@@ -30,3 +58,20 @@ function discountTimer(){
     }
 }
 const interval = setInterval(discountTimer, 1000)
+
+// MAKING THE FA-HEART CHANGE WHEN CLICKED ---- 15 September 2024
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.like-product button').forEach(button => {
+        button.addEventListener('click', () => {
+            const heartIcon = button.querySelector('i');
+            heartIcon.classList.toggle('fa-regular');
+            heartIcon.classList.toggle('fa-solid');
+        });
+    });
+    const discountLike = document.querySelector('.like button')
+    discountLike.addEventListener('click', () => {
+        const heartIcon = discountLike.querySelector('i');
+        heartIcon.classList.toggle('fa-regular');
+        heartIcon.classList.toggle('fa-solid');
+    })
+});
